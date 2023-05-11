98385093240f5c48b005b4b320ef647adb92b208
nirgluzman <nir.gluzman@gmail.com>
2023-05-11 20:30:44 +0200

    add useForm basic operations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import { useForm } from 'react-hook-form';
+
 import './App.css';
 
 function App() {
+  const {
+    register,
+    formState: { errors },
+    watch,
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      username: 'Bob',
+      password: '123',
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <p>
-        <input placeholder='username' type='text' />
+        <input
+          {...register('username', { required: 'This field is required' })}
+          placeholder='username'
+          type='text'
+        />
       </p>
+      {errors.username?.message}
       <p>
-        <input placeholder='password' type='password' />
+        <input
+          {...register('password')}
+          placeholder='password'
+          type='password'
+        />
       </p>
+      <p>{watch('username')}</p>
       <input type='submit' />
     </form>
   );
